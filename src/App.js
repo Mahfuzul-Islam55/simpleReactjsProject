2e57b05242f44333c8614af8d6edc43c6e7408a0
Mahfuzul-Islam55 <43821861+Mahfuzul-Islam55@users.noreply.github.com>
2020-12-09 03:41:29 +0600

    making top navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TopBanner from "./components/TopBanner/TopBanner";
 import Services from "./components/Services/Services";
+import TopNavigation from "./components/TopNavigation/TopNavigation";
 
 
 function App() {
   return (
     <div>
+<TopNavigation/>
 <TopBanner/>
 <Services/>
 
