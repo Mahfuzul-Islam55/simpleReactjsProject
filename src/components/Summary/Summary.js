a3a75cbc148257337c642047db88fa33da5cbdb2
Mahfuzul-Islam55 <43821861+Mahfuzul-Islam55@users.noreply.github.com>
2020-12-11 02:57:11 +0600

    adding count-up animation

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -2,7 +2,8 @@ import React, {Component,Fragment} from 'react';
 import {Button, Card, Col, Container, Row} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCheckCircle} from "@fortawesome/free-solid-svg-icons";
-
+import CountUp from 'react-countup';
+import VisibilitySensor from "react-visibility-sensor";
 class Summary extends Component {
     render() {
         return (
@@ -15,13 +16,35 @@ class Summary extends Component {
                                 <Col lg={8} md={6} sm={12}>
                                     <Row className="countSection">
                                          <Col>
-                                             <h1 className="countNumber">100</h1>
+                                             <h1 className="countNumber">
+
+                                                 <CountUp start={0} end={100}>
+                                                     {({ countUpRef, start }) => (
+                                                         <VisibilitySensor onChange={start} delayedCall>
+                                                             <span ref={countUpRef} />
+                                                         </VisibilitySensor>
+                                                     )}
+                                                 </CountUp>
+
+
+
+                                             </h1>
                                              <h4 className="countTitle">Total Projects</h4>
                                              <hr className="bg-white w-25"/>
                                          </Col>
 
                                         <Col>
-                                            <h1 className="countNumber">100</h1>
+                                            <h1 className="countNumber">
+
+                                                <CountUp start={0} end={100}>
+                                                    {({ countUpRef, start }) => (
+                                                        <VisibilitySensor onChange={start} delayedCall>
+                                                            <span ref={countUpRef} />
+                                                        </VisibilitySensor>
+                                                    )}
+                                                </CountUp>
+
+                                            </h1>
                                             <h4 className="countTitle">Total Clients</h4>
                                             <hr className="bg-white w-25"/>
                                         </Col>
