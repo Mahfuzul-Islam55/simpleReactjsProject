d639cbfb5d5af661b79e00fd24a01e2a0906c34d
Mahfuzul-Islam55 <43821861+Mahfuzul-Islam55@users.noreply.github.com>
2020-12-11 02:36:13 +0600

    summary section

--- src/components/Analysis/Analysis.js
+++ src/components/Analysis/Analysis.js
@@ -41,7 +41,6 @@ class Analysis extends Component {
 
 
                         </Col>
-
                         <Col lg={6} md={12} sm={12}>
                         <p className="text-justify des">To build native android apps i use Java as well as kotline mainly. React JS is used for cross platform mobile application. I use MySQL database for relational database system. To build NoSQL application i use MongoDB. Firebase database system is used where it is necessary to provide realtime data flow facilities.</p>
                             <p className="text-justify des"> I always build dynamic application. Admin panel is the heart of such kinds of application. I always try to provide sufficient features in admin panel to dominate application. According to client demand I use PHP OOP, CodeIgniter and Laravel to build admin panel section. </p>
---
+++ src/components/Summary/Summary.js
@@ -0,0 +1,56 @@
+import React, {Component,Fragment} from 'react';
+import {Button, Card, Col, Container, Row} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCheckCircle} from "@fortawesome/free-solid-svg-icons";
+
+class Summary extends Component {
+    render() {
+        return (
+            <Fragment>
+
+                <Container fluid={true} className="SummaryBanner SummarySection p-0" >
+                    <div className="SummaryBannerOverlay">
+                        <Container className="text-center">
+                            <Row>
+                                <Col lg={8} md={6} sm={12}>
+                                    <Row className="countSection">
+                                         <Col>
+                                             <h1 className="countNumber">100</h1>
+                                             <h4 className="countTitle">Total Projects</h4>
+                                             <hr className="bg-white w-25"/>
+                                         </Col>
+
+                                        <Col>
+                                            <h1 className="countNumber">100</h1>
+                                            <h4 className="countTitle">Total Clients</h4>
+                                            <hr className="bg-white w-25"/>
+                                        </Col>
+
+                                    </Row>
+                                </Col>
+
+                                <Col lg={4} md={6} sm={12}>
+                                    <Card className="workCard">
+                                        <Card.Body>
+                                            <Card.Title className="cardTitle text-justify">How i Work</Card.Title>
+                                            <Card.Text>
+                                                <p className="cardSubTitle text-justify"><FontAwesomeIcon className="iconBullet" icon={faCheckCircle} /> Requirement Gathering </p>
+                                                <p className="cardSubTitle text-justify"><FontAwesomeIcon className="iconBullet" icon={faCheckCircle} /> System Analysis </p>
+                                                <p className="cardSubTitle text-justify"><FontAwesomeIcon className="iconBullet" icon={faCheckCircle} /> Coding Testing</p>
+                                                <p className="cardSubTitle text-justify"><FontAwesomeIcon className="iconBullet" icon={faCheckCircle} /> Implementation</p>
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+
+                            </Row>
+                        </Container>
+                    </div>
+                </Container>
+                
+            </Fragment>
+        );
+    }
+}
+
+export default Summary;
