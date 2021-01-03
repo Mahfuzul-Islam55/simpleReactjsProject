f3be8eb5f9713a46a0298f628a25796fb353ac87
Mahfuzul-Islam55 <43821861+Mahfuzul-Islam55@users.noreply.github.com>
2021-01-04 00:05:51 +0600

    setting page top and about page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import HomePage from "./pages/HomePage";
+import PageTop from "./components/PageTop/PageTop";
+import TopNavigation from "./components/TopNavigation/TopNavigation";
+import AboutDescription from "./components/AboutDescription/AboutDescription";
+import AboutPage from "./pages/AboutPage";
+import CoursesPage from "./pages/CoursesPage";
+import PortfolioPage from "./pages/PortfolioPage";
 function App() {
   return (
     <div>
-        <HomePage/>
+            <PortfolioPage/>
     </div>
   );
 }
